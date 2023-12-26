4f3ba6611b897c2b19dd0058e57e1d4cadd6e306
Luan Himmlisch <luan@himmlisch.com.mx>
2023-12-25 22:59:54 -0600

    Created base component

---
+++ src/Components/WorldComponent.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace WeblaborMx\WorldUi\Components;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+use WeblaborMx\World\World;
+use WireUi\View\Components\Select;
+
+abstract class WorldComponent extends Select
+{
+    protected int $cacheMinutes = 1;
+
+    abstract protected function endpoint(): string;
+
+    public function __construct()
+    {
+        parent::__construct(
+            optionKeyValue: true,
+            options: $this->getOptions()->mapWithKeys(fn ($v) => [$v['id'] => $v['name']])
+        );
+    }
+
+    private function cacheKey(): string
+    {
+        return md5("worldui.native-select:{$this->endpoint()}");
+    }
+
+    public function getOptions(): Collection
+    {
+        $key = $this->cacheKey();
+
+        /** @var Collection */
+        $options = Cache::remember(
+            $key,
+            now()->addMinutes($this->cacheMinutes),
+            fn () => collect(World::getClient()->makeSafeCall($this->endpoint()))
+        );
+
+        if ($options->isEmpty()) {
+            Cache::forget($key);
+        }
+
+        return $options;
+    }
+}
